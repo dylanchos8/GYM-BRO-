47eff3feb54c731f88409bd6a60bf813a874d235
dylanchos8 <dylancurreacerinza@gmail.com>
2025-06-06 07:49:58 -0500

    primer commit del proyecto GYM-BRO

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import cors from 'cors';
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import authRoutes from './routes/authRoutes.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json()); // necesario para leer req.body si usas JSON
+
+// Para obtener __dirname con ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Servir archivos estáticos desde el frontend
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+// Montar las rutas de autenticación
+app.use('/api', authRoutes); // <-- Añadido
+
+// Ruta raíz que sirve login.html por defecto
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/inicio/inicio.html'));
+});
+
+app.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
+});
