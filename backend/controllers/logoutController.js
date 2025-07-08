824d97e18cfc6c465cbdb76f50e66cc4a8594a8a
Dylan_Was_Here <131723636+dylanchos8@users.noreply.github.com>
2025-07-08 11:06:58 -0500

    Update logoutController.js

--- backend/controllers/logoutController.js
+++ backend/controllers/logoutController.js
@@ -1,1 +1,9 @@
-
+export const cerrarSesion = (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      return res.status(500).json({ mensaje: 'Error al cerrar sesión' });
+    }
+    res.clearCookie('connect.sid'); // Nombre por defecto de la cookie de sesión
+    res.status(200).json({ mensaje: 'Sesión cerrada correctamente' });
+  });
+};
