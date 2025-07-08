08857b41511ec0aa99c7194da9afb0c1972575af
Dylan_Was_Here <131723636+dylanchos8@users.noreply.github.com>
2025-07-08 11:37:38 -0500

    Delete GYM-BRO--main directory

--- GYM-BRO--main/frontend/inicio/js/logout.js
+++
@@ -1,8 +0,0 @@
-// logout.js
-document.addEventListener('DOMContentLoaded', () => {
-  // Eliminar todos los datos del usuario almacenados
-  localStorage.clear();
-
-  // Redireccionar al login
-  window.location.href = '/frontend/inicio/pages/inicio.html';
-});
