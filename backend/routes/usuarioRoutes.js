2394dc32133e7c37f8d4dd4c2cd16d1403e48d70
Dylan_Was_Here <131723636+dylanchos8@users.noreply.github.com>
2025-07-08 11:15:24 -0500

    Update usuarioRoutes.js
    actualizacion de usuarioRoutes.js para la recuperacion de contraseña del usuario

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,11 +1,13 @@
  //routes definen los puntos de entrada GET y POST, además llama a los metodos
 import express from 'express';
-import { obtenerUsuarios, crearUsuario, eliminarUsuarioPorId} from '../controllers/usuarioController.js';
+import { obtenerUsuarios, crearUsuario, recuperarContrasena, eliminarUsuarioPorId} from '../controllers/usuarioController.js';
+
 
 const router = express.Router();
 
 router.get('/', obtenerUsuarios);
 router.post('/', crearUsuario);
+router.post('/recuperar-contrasena', recuperarContrasena);
 router.delete('/:id', eliminarUsuarioPorId);
 
 export default router;
